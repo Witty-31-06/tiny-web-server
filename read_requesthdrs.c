ea16d49d3bf722c3e9405fae2431c69457e5c282
Witty-31-06 <92244610+Witty-31-06@users.noreply.github.com>
2024-08-20 22:56:49 +0530

    removed comments

--- read_requesthdrs.c
+++ read_requesthdrs.c
@@ -3,10 +3,9 @@ void read_reqesthdrs(rio_t *rio)
 {
     char buf[MAXLINE];
     rio_readlineb(rio, buf, MAXLINE);
-    while(strcmp(buf, "\r\n")) //last line empty
+    while(strcmp(buf, "\r\n"))
     {
         rio_readlineb(rio, buf, MAXLINE);
-        // printf("%s", buf);
     }
     return;
 }
